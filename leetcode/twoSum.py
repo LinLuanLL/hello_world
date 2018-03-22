5fd3a4d075f580f1eb6271a6dfa1fedbc08aa7cc
LinLuanLL <952054523@qq.com>
2018-03-22 18:57:01 +0800

    first commit

---
+++ leetcode/twoSum.py
@@ -0,0 +1,7 @@
+def twoSum(nums, target):
+    for i in range(0,len(nums)):
+        if target - nums[i] in nums:
+            return [i,nums.index(target - nums[i],i+1)]
+nums = [2, 7, 11, 15]
+target = 9
+(a,b) = twoSum(nums,target)
